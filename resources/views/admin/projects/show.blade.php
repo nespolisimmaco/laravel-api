04af8ff2ddf571d51e5199a5dd9a67b9a4dc0709
nespolisimmaco <snespoli@yahoo.com>
2023-06-08 18:12:19 +0200

    Add: show the technologies used in a project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 class="text-center">{{ $project->title }}</h1>
-    <div class="d-flex justify-content-between">
+    <div class="d-flex justify-content-between my-3">
         @if ($project->type)
             <div>Tipo: {{ $project->type->name }}</div>
         @else
@@ -10,10 +10,18 @@
         @endif
         <div>{{ $project->slug }}</div>
     </div>
-    <h3>Contenuto</h3>
+    <h4>Contenuto</h4>
     <p class="mt-4">{{ $project->content }}</p>
-    <h3>Descrizione</h3>
+    <h4>Descrizione</h4>
     <p class="mt-4">{{ $project->description }}</p>
+    <div class="mt-4">
+        <h4>Tecnologie</h4>
+        @forelse ($project->technologies as $technology)
+            <span>{{ $technology->name }} {{ $loop->last ? '' : ',' }}</span>
+        @empty
+            <span>Nessuna tecnologia inserita</span>
+        @endforelse
+    </div>
     <a href="{{ route('admin.projects.index') }}" class="btn btn-primary my-3">Torna alla lista</a>
     <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-warning my-3 mx-1 d-inline-block">Modifica</a>
     <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
