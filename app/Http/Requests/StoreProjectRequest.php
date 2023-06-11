db1d9e36dbfae35a6626f30eadff12f4046298e6
nespolisimmaco <snespoli@yahoo.com>
2023-06-11 17:59:15 +0200

    Add: combine technologies when creating a project

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -28,7 +28,8 @@ class StoreProjectRequest extends FormRequest
             'title'=>['required', Rule::unique('projects')],
             'content'=>'nullable',
             'description'=>'nullable',
-            'type_id'=>['nullable', 'exists:types,id']
+            'type_id'=>['nullable', 'exists:types,id'],
+            'techs'=>['required', 'exists:technologies,id']
         ];
     }
 }
